6708d8f81c38a822e7b10ed7a78f51c3c3bc0098
RevDonz <aprilio842@gmail.com>
2022-04-09 09:39:13 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnal7_1302204051
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
